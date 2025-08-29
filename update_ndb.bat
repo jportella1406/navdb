01672f7e55e9ec1a27cec17592ba88727c27c089
jportella <jportella1406@gmail.com>
2025-08-29 09:57:33 -0700

    adicion de host_script y update_ndb

---
+++ update_ndb.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set CAE_HOST=10.102.80.1
+set "LOGDIR=C:\Users\josep\OneDrive\Desktop\CAE\navdb\actualizar_ndb\200 01\logs"
+if not exist "%LOGDIR%" mkdir "%LOGDIR%"
+
+echo Starting FTP Process...
+
+:: === Timestamp for log ===
+for /f "tokens=2 delims==" %%a in ('wmic os get localdatetime /value') do set "dt=%%a"
+:: dt = AAAAMMDDhhmmss.xxx
+set "YYYY=%dt:~0,4%"
+set "MM=%dt:~4,2%"
+set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%"
+set "NN=%dt:~10,2%"
+set "SS=%dt:~12,2%"
+set "STAMP=%YYYY%-%MM%-%DD%_%HH%-%NN%-%SS%"
+
+ftp -n -s:host_script.txt %CAE_HOST% > "%LOGDIR%\update_%STAMP%.log" 2>&1
+
+if errorlevel 1 (
+  echo [ERROR] Fallo el FTP. Revisa update_%STAMP%.log
+  exit /b 5
+)
